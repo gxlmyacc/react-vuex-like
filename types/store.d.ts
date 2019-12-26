a2473e5cee5f132c21897e2fd8de8fa3e7b26f29
guoxl <gxl.mymail@163.com>
2019-12-26 18:47:38 +0800

    some changes

--- types/store.d.ts
+++ types/store.d.ts
@@ -6,11 +6,11 @@ export declare type ActionListener = (event: {
     type: string;
     payload: any;
 }, state: Partial<any>) => void;
-export interface StoreBase<S> {
+export interface StoreBase {
     state: Partial<any>;
     getters: Partial<any>;
 }
-export interface StoreModule<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase<StoreModule> = {
+export interface StoreModule<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase = {
     state: Partial<any>;
     getters: Partial<any>;
 }, M extends Partial<StoreModule> = {
@@ -31,18 +31,18 @@ export interface StoreModule<S extends Partial<any> = Partial<any>, G extends Pa
         App: any;
     }): void;
 }
-export declare type StorePlugin<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase<Store> = {
+export declare type StorePlugin<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase = {
     state: Partial<any>;
     getters: Partial<any>;
 }, M extends Partial<StoreModule> = {
     [key: string]: StoreModule;
 }> = (store: Store<S, G, P, M>) => void;
-declare class Store<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase<StoreModule> = {
+declare class Store<S extends Partial<any> = Partial<any>, G extends Partial<any> = Partial<any>, P extends StoreBase = {
     state: Partial<any>;
     getters: Partial<any>;
 }, M extends Partial<StoreModule> = {
     [key: string]: StoreModule;
-}> implements StoreBase<Store> {
+}> implements StoreBase {
     state: P['state'] & S & {
         [K in keyof M]: {
             [K in keyof M['state']]?: M['state'][K];
@@ -56,32 +56,54 @@ declare class Store<S extends Partial<any> = Partial<any>, G extends Partial<any
     readonly moduleName: string;
     readonly namespaced: boolean;
     readonly strict: boolean;
-    private root;
-    private parent?;
-    private mutationListeners;
-    private actionListeners;
-    private mutations;
-    private actions;
-    private modules;
-    private plugins;
-    private extends?;
-    private _commiting;
-    private _module;
-    private _state;
-    private _getters;
+    protected root: any;
+    protected parent?: any;
+    protected mutationListeners: MutationListener[];
+    protected actionListeners: ActionListener[];
+    protected mutations: {
+        [event: string]: (state: P['state'] & S & {
+            [K in keyof M]: {
+                [K in keyof M['state']]?: M['state'][K];
+            };
+        }, payload: any, parent?: Store, root?: Store) => any;
+    };
+    protected actions: {
+        [event: string]: ({ state, getters, commit }: {
+            state: P['state'] & S & {
+                [K in keyof M]: {
+                    [K in keyof M['state']]?: M['state'][K];
+                };
+            };
+            getters: P['getters'] & G & {
+                [K in keyof M]: {
+                    [K in keyof M['getters']]?: M['getters'][K];
+                };
+            };
+            commit: (event: string, payload: any) => any;
+        }) => any;
+    };
+    protected modules: {
+        [moduleName: string]: Store;
+    };
+    protected plugins: StorePlugin<S, G, P, M>[];
+    protected extends?: Store;
+    protected _commiting: boolean;
+    protected _module: StoreModule<S, G, P, M>;
+    protected _state: S;
+    protected _getters: G;
     constructor(module: StoreModule<S, G, P, M>, parent?: Store, root?: Store, moduleName?: string);
     get namespace(): string;
-    _createState(state?: Partial<any>): Partial<any>;
-    _createGetters(getters?: Partial<any>): G;
-    _getModuleKey(moduleName: string, key: string): string;
-    _mergeState(moduleName: string, state: Partial<any>): void;
-    _mergeGetters(moduleName: string, getters: Partial<any>): void;
-    _mergeMutations(moduleName: string, mutations: Partial<any>): void;
-    _mergeActions(moduleName: string, actions: Partial<any>): void;
-    _removeState(key: string): void;
-    _removeGetter(key: string): void;
-    _removeMutation(key: string): void;
-    _removeAction(key: string): void;
+    protected _createState(state?: Partial<any>): Partial<any>;
+    protected _createGetters(getters?: Partial<any>): G;
+    protected _getModuleKey(moduleName: string, key: string): string;
+    protected _mergeState(moduleName: string, state: Partial<any>): void;
+    protected _mergeGetters(moduleName: string, getters: Partial<any>): void;
+    protected _mergeMutations(moduleName: string, mutations: Partial<any>): void;
+    protected _mergeActions(moduleName: string, actions: Partial<any>): void;
+    protected _removeState(key: string): void;
+    protected _removeGetter(key: string): void;
+    protected _removeMutation(key: string): void;
+    protected _removeAction(key: string): void;
     resetState(): void;
     replaceState(state?: {}): void;
     registerModule(moduleName: string, module?: StoreModule): Store<Partial<any>, Partial<any>, {
